a074b321734f0145ed5873f1ae06727950e3e993
Sam Hubner <K038639@eltham-college.org.uk>
2021-05-13 12:20:57 +0100

    Add project files.

---
+++ Queues Tas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Queues_Tas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Queue one = new Queue();
+
+            one.add("one");
+            one.add("two");
+            one.add("three");
+            one.add("four");
+            one.add("five");
+            one.add("six");
+            one.add("seven");
+            one.add("eight");
+
+            one.Print();
+
+            one.pop();
+            Console.WriteLine();
+
+            one.Print();
+            one.pop();
+            Console.WriteLine();
+
+            one.size();
+            Console.ReadLine();
+
+            one.Print();
+            one.pop();
+            Console.WriteLine();
+
+            one.Print();
+        }
+    }
+}
---
+++ Queues Tas/Queue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queues_Tas
+{
+    class Queue
+    {
+        int front;
+        int back;
+        int count;
+        string[] Q;
+
+        public Queue()
+        {
+            front = 0;back = 0;count = 0;
+
+            Q = new string[8];
+        }
+
+        public void add(string x)
+        {
+            if (count < 8)
+            {
+                Q[back] = x;
+
+                back = back + 1;
+                count = count + 1;
+            }
+            else
+            {
+                Console.WriteLine("Queue is full");
+            }
+        }
+
+        public void pop()
+        {
+            if (count > 0)
+            {
+                Q[front] = null;
+                front = front + 1;
+                count = count - 1;
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty");
+            }
+        }
+        public void Print()
+        {
+            for (int x = 0; x < Q.Length; x = x + 1)
+            {
+                Console.WriteLine(Q[x]);
+            }
+        }
+
+        public int size()
+        {
+            Console.WriteLine(Convert.ToString(count));
+            return count;
+        }
+    }
+}
